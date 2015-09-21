53a1f203feb151d1c5474cd558a66c2d5aba9c77
Eros Pedrini <eros.pedrini@gmail.com>
2015-09-21 22:28:06 +0000

    Execise A (OK) and Exercise B (KO)

--- src/FileReaderSpout.java
+++ src/FileReaderSpout.java
@@ -2,8 +2,12 @@
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Map;
+import java.util.Scanner;
+import java.lang.InterruptedException;
 
 import backtype.storm.spout.SpoutOutputCollector;
 import backtype.storm.task.TopologyContext;
@@ -12,10 +16,27 @@ import backtype.storm.topology.OutputFieldsDeclarer;
 import backtype.storm.tuple.Fields;
 import backtype.storm.tuple.Values;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
 public class FileReaderSpout implements IRichSpout {
+  private final static Logger LOG = LoggerFactory.getLogger(FileReaderSpout.class);
+
+  private transient final Scanner scanner;
+
   private SpoutOutputCollector _collector;
   private TopologyContext context;
 
+
+  public FileReaderSpout(final String path) throws FileNotFoundException {
+	try {
+		scanner = new Scanner(new File(path));
+	} catch (FileNotFoundException e) {
+		LOG.error("the file '{}' doesn't exist", path);
+		throw e;	
+	}
+  }
 
   @Override
   public void open(Map conf, TopologyContext context,
@@ -27,6 +48,8 @@ public class FileReaderSpout implements IRichSpout {
 
 
     ------------------------------------------------- */
+
+    // nothing to do... all in the constructor....
 
     this.context = context;
     this._collector = collector;
@@ -43,7 +66,16 @@ public class FileReaderSpout implements IRichSpout {
 
     ------------------------------------------------- */
 
-
+    if (scanner.hasNext()) {
+	_collector.emit( new Values(scanner.nextLine()) );
+    } {
+	LOG.info("the file is finished");
+	try {	
+		Thread.sleep( 2 * 60 * 1000);
+	} catch (InterruptedException e) {
+		LOG.info("Spout nextTuple sleep interrupted");
+	}	
+    }
   }
 
   @Override
@@ -62,13 +94,17 @@ public class FileReaderSpout implements IRichSpout {
 
     ------------------------------------------------- */
 
+    if (null != scanner) {
+	scanner.close();
+    }
+
   }
 
 
   @Override
   public void activate() {
   }
-
+  
   @Override
   public void deactivate() {
   }
