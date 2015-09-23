e6382a04b8d95183838e8e6932e9e98ed09f2ade
Eros Pedrini <eros.pedrini@gmail.com>
2015-09-23 23:42:28 +0200

    Execise A (OK) and, also, Exercise B (OK)

--- src/FileReaderSpout.java
+++ src/FileReaderSpout.java
@@ -1,5 +1,5 @@
-
 import java.io.BufferedReader;
+import java.io.Closeable;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.File;
@@ -17,26 +17,23 @@ import backtype.storm.utils.Utils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
 public class FileReaderSpout implements IRichSpout {
   private final static Logger LOG = LoggerFactory.getLogger(FileReaderSpout.class);
 
   private final String filepath;
-
   private boolean isCompleted = false;
 
   private SpoutOutputCollector collector;
   private TopologyContext context;
 
-  public FileReaderSpout(final String filepath) throws FileNotFoundException {
-	this.filepath = filepath;
+  public FileReaderSpout(final String filepath) {
+    this.filepath = filepath;
   }
 
   @Override
   public void open(Map conf,
-		   TopologyContext context,
+                   TopologyContext context,
                    SpoutOutputCollector collector) {
-
     this.context = context;
     this.collector = collector;
   }
@@ -44,37 +41,49 @@ public class FileReaderSpout implements IRichSpout {
   @Override
   public void nextTuple() {
     if (!isCompleted) {
-	try {
-		final FileReader fileReader = new FileReader(filepath);
-		final BufferedReader reader = new BufferedReader(fileReader);
-		String line;
-		while ( null != (line = reader.readLine()) ) {
-			collector.emit( new Values(line) );
-		}
-		
-		LOG.info("The file {} is finished", filepath);
-		isCompleted = true;
-
-		reader.close();
-	} catch (IOException e) {
-		LOG.error("Generic exception during tuple retrieving: {}", e);
-	} finally {
-		isCompleted = true;
-	}
+      Closeable closeable = null;
+      try {
+        // both the FileReader and BufferedReader instances are directly
+        // created and initialized here because they don't support
+        // Serialization.
+        final FileReader fileReader = new FileReader(filepath);
+        closeable = fileReader;
+        
+        final BufferedReader reader = new BufferedReader(fileReader);
+        closeable = reader;
+        
+        String line;
+        while ( null != (line = reader.readLine()) ) {
+          collector.emit( new Values(line) );
+        }
+        
+        LOG.info("The file {} is finished", filepath);
+      } catch (IOException e) {
+        LOG.error("Generic exception during tuple retrieving: {}", e);
+      } finally {
+        isCompleted = true;
+        
+		    if (null != closeable) {
+		      try {
+		        closeable.close();
+		      } catch (IOException e) {
+		        LOG.error("Error during reader ({}) closing: {}", closeable, e);
+		      }
+        }
+      }
     } else {
-    	Utils.sleep(100);
+      Utils.sleep(100);
     }
   }
 
   @Override
   public void declareOutputFields(OutputFieldsDeclarer declarer) {
-
     declarer.declare(new Fields("word"));
-
   }
 
   @Override
   public void close() {
+    // nothing to do... all already closed
   }
 
 
