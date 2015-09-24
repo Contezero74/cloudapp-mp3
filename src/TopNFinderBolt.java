d540d5cba4c5ad849aa7240c9ce8f1383a05b81a
Eros Pedrini <eros.pedrini@gmail.com>
2015-09-24 15:44:37 +0000

    Exercise D: first version. It seems to work but I'm not sure

--- src/TopNFinderBolt.java
+++ src/TopNFinderBolt.java
@@ -6,12 +6,15 @@ import backtype.storm.tuple.Tuple;
 import backtype.storm.tuple.Values;
 
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.TreeSet;
 
 /**
  * a bolt that finds the top n words.
  */
 public class TopNFinderBolt extends BaseBasicBolt {
   private HashMap<String, Integer> currentTopWords = new HashMap<String, Integer>();
+  private TreeSet<Pair<Integer, String>> countToWord = new TreeSet<Pair<Integer, String>>();
   private int N;
 
   private long intervalToReport = 20;
@@ -23,18 +26,26 @@ public class TopNFinderBolt extends BaseBasicBolt {
 
   @Override
   public void execute(Tuple tuple, BasicOutputCollector collector) {
- /*
-    ----------------------TODO-----------------------
-    Task: keep track of the top N words
-
-
-    ------------------------------------------------- */
-
+    final String word = tuple.getString(0);
+    final Integer count = tuple.getInteger(1);
+   
+    countToWord.add(new Pair<Integer, String>(count, word));
+    if (countToWord.size() > N) {
+	countToWord.remove(countToWord.last());
+    } 
 
     //reports the top N words periodically
     if (System.currentTimeMillis() - lastReportTime >= intervalToReport) {
-      collector.emit(new Values(printMap()));
-      lastReportTime = System.currentTimeMillis();
+	currentTopWords.clear();
+
+	Iterator<Pair<Integer, String>> i = countToWord.iterator();
+	while( i.hasNext() ) {
+		final Pair<Integer, String> current = i.next();
+		currentTopWords.put(current.second, current.first);
+	}
+	
+	collector.emit(new Values(printMap()));
+	lastReportTime = System.currentTimeMillis();
     }
   }
 
@@ -58,4 +69,57 @@ public class TopNFinderBolt extends BaseBasicBolt {
     return stringBuilder.toString();
 
   }
+}
+
+class Pair<A extends Comparable<? super A>,
+        B extends Comparable<? super B>>
+        implements Comparable<Pair<A, B>> {
+
+    public final A first;
+    public final B second;
+
+    public Pair(A first, B second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public static <A extends Comparable<? super A>,
+            B extends Comparable<? super B>>
+    Pair<A, B> of(A first, B second) {
+        return new Pair<A, B>(first, second);
+    }
+
+    @Override
+    public int compareTo(Pair<A, B> o) {
+        int cmp = o == null ? 1 : -(this.first).compareTo(o.first);
+        return cmp == 0 ? (this.second).compareTo(o.second) : cmp;
+    }
+
+    @Override
+    public int hashCode() {
+        return 31 * hashcode(first) + hashcode(second);
+    }
+
+    private static int hashcode(Object o) {
+        return o == null ? 0 : o.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof Pair))
+            return false;
+        if (this == obj)
+            return true;
+        return equal(first, ((Pair<?, ?>) obj).first)
+                && equal(second, ((Pair<?, ?>) obj).second);
+    }
+
+    private boolean equal(Object o1, Object o2) {
+        return o1 == o2 || (o1 != null && o1.equals(o2));
+    }
+
+    @Override
+    public String toString() {
+        return "(" + first + ", " + second + ')';
+    }
 }
